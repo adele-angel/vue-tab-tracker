d13783832755cf5ce52e946058a69196885b24f0
adele-angel <adeler.ang@gmail.com>
2020-03-07 04:23:04 +0200

    Updates

---
+++ precache-manifest.fce4e66a218bbc8e7450845bdfdfc5b3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4df866b20328551c29",
+    "url": "/vue-tab-tracker/css/app.7b4a4459.css"
+  },
+  {
+    "revision": "4bbaf8b94d7ed3248893",
+    "url": "/vue-tab-tracker/css/chunk-3936498c.0bda4a20.css"
+  },
+  {
+    "revision": "09d6e18a7184d92b859c",
+    "url": "/vue-tab-tracker/css/chunk-786ca0a8.ca6df252.css"
+  },
+  {
+    "revision": "f439daa5045164dd7856",
+    "url": "/vue-tab-tracker/css/chunk-7af5931c.ca6df252.css"
+  },
+  {
+    "revision": "d2fa397971062d219b31",
+    "url": "/vue-tab-tracker/css/chunk-9722edc2.36ca82f6.css"
+  },
+  {
+    "revision": "60f3a02825168eb9cf1f",
+    "url": "/vue-tab-tracker/css/chunk-d9b69d7a.5c0afcf4.css"
+  },
+  {
+    "revision": "ceb62cc02920e59bc9d5",
+    "url": "/vue-tab-tracker/css/chunk-vendors.ae0d394b.css"
+  },
+  {
+    "revision": "1f4aab48500f2929b0cf5b31aa3497e6",
+    "url": "/vue-tab-tracker/img/tabs.1f4aab48.png"
+  },
+  {
+    "revision": "0864bb11befa5e04fcb6d9b2b8d735c3",
+    "url": "/vue-tab-tracker/index.html"
+  },
+  {
+    "revision": "cc4df866b20328551c29",
+    "url": "/vue-tab-tracker/js/app.ecc669fe.js"
+  },
+  {
+    "revision": "e95652f75383067db5f3",
+    "url": "/vue-tab-tracker/js/chunk-2d22d746.993a7033.js"
+  },
+  {
+    "revision": "4bbaf8b94d7ed3248893",
+    "url": "/vue-tab-tracker/js/chunk-3936498c.39357276.js"
+  },
+  {
+    "revision": "a1ec4836c2fbdbf1f694",
+    "url": "/vue-tab-tracker/js/chunk-40da3653.9ef77760.js"
+  },
+  {
+    "revision": "fc49b326b28616b907c2",
+    "url": "/vue-tab-tracker/js/chunk-40db51ec.cc19e62b.js"
+  },
+  {
+    "revision": "09d6e18a7184d92b859c",
+    "url": "/vue-tab-tracker/js/chunk-786ca0a8.114d5d9b.js"
+  },
+  {
+    "revision": "f439daa5045164dd7856",
+    "url": "/vue-tab-tracker/js/chunk-7af5931c.257d4d31.js"
+  },
+  {
+    "revision": "d2fa397971062d219b31",
+    "url": "/vue-tab-tracker/js/chunk-9722edc2.687eb09d.js"
+  },
+  {
+    "revision": "a885f585adb3ef78d551",
+    "url": "/vue-tab-tracker/js/chunk-b230d44e.76710b21.js"
+  },
+  {
+    "revision": "60f3a02825168eb9cf1f",
+    "url": "/vue-tab-tracker/js/chunk-d9b69d7a.e5781bb3.js"
+  },
+  {
+    "revision": "ceb62cc02920e59bc9d5",
+    "url": "/vue-tab-tracker/js/chunk-vendors.6ed46cec.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/vue-tab-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-tab-tracker/robots.txt"
+  }
+]);
